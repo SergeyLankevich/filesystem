dfe3f19d91d014413df1a9b1b1f89edd0d7148c5
SergeyLankevich <43179675+SergeyLankevich@users.noreply.github.com>
2020-03-09 16:41:21 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,43 @@
 import os
 
 
+def dir_watch(path):
+    print(os.listdir(path))
+
+
+def move_up():
+    parent_dir = os.path.dirname(os.getcwd())
+    os.chdir(parent_dir)
+
+
+def move_down(subdirectory):
+    sublist = os.listdir(os.getcwd())
+    if subdirectory in sublist:
+        os.chdir(subdirectory)
+
+
+def file_counter(path):
+    count = 0
+    for filename in os.listdir(path):
+        f_path = os.path.join(path, filename)
+        if os.path.isfile(f_path):
+            count += 1
+        elif os.path.isdir(f_path):
+            count += file_counter(f_path)
+    return count
+
+
+def file_scales(path):
+    count = 0
+    for filename in os.listdir(path):
+        f_path = os.path.join(path, filename)
+        if os.path.isfile(f_path):
+            count += os.path.getsize(f_path)
+        elif os.path.isdir(f_path):
+            count += file_counter(f_path)
+    return count
+
+
 def mask(name, name_with_reg):
     start1 = 0
     end1 = 0
@@ -60,4 +97,56 @@ def find(file_name_with_reg):
         find(file_name_with_reg)
 
 
-find('*.txt')
+def closure():
+    exit()
+
+
+def menu():
+    print(os.getcwd())
+    command = 1
+    try:
+        while command != 7:
+            command = int(input(
+            '1.Просмотр Каталога\n2.На уровень вверх\n3.На уровень вниз.'
+            '\n4.Количество файлов\n5.Размер текущего каталога\n'
+            '6.Поиск файла\n7.Выход из программы\n: '))
+
+            if command == 1:
+                try:
+                    p = input('Folder: ')
+                    dir_watch(p)
+                except:
+                    print('Folder Not Found')
+            if command == 2:
+                try:
+                    move_up()
+                except:
+                    print('Folder Not Found')
+            if command == 3:
+                try:
+                    p = input('Folder: ')
+                    move_down(p)
+                except:
+                    print('Folder Not Found')
+            if command == 4:
+                try:
+                    p = input('Folder: ')
+                    file_counter(p)
+                except:
+                    print('Folder Not Found')
+            if command == 5:
+                try:
+                    p = input('Folder: ')
+                    file_scales(p)
+                except:
+                    print('Folder Not Found')
+            if command == 6:
+                try:
+                    p = input('File Name with Reg.: ')
+                    find(p)
+                except:
+                    print('Folder Not Found')
+    except:
+        print('File/Folder Not Found')
+
+menu()
